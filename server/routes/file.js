7dcf416de690764ef615cc07be07d991c5082903
1dynamite <turaevshokhrukh7@gmail.com>
2022-03-03 23:05:12 +0500

    fix: add id params for file identification

--- server/routes/file.js
+++ server/routes/file.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const handler = require('../handlers/file');
 
+router.post('/', handler.upload);
 
-router.route('/')
+router.route('/:id')
   .get(handler.read)
-  .post(handler.upload)
   .delete(handler.remove);
   
 
